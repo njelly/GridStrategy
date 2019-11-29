128127b6eee54bc3274e89b892e3fca1d9f0e1f3
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2019-11-28 18:07:16 -0700

    made a git error, trying to fix

--- Assets/_GridStrategy/Scripts/InGameController.cs
+++ Assets/_GridStrategy/Scripts/InGameController.cs
@@ -12,34 +12,46 @@ using UnityEngine;
 
 namespace Tofunaut.GridStrategy
 {
+    // --------------------------------------------------------------------------------------------
     public class InGameController : ControllerBehaviour
     {
+
+        // --------------------------------------------------------------------------------------------
         private static class State
         {
-            public const string Loading = "loading";
+            public const string Loading = "Loading";
             public const string InGame = "InGame";
         }
 
         private TofuStateMachine _stateMachine;
 
+        // --------------------------------------------------------------------------------------------
         private void Awake()
         {
             _stateMachine = new TofuStateMachine();
-            _stateMachine.Register(State.Loading, LoadingEnter, null, null);
+            _stateMachine.Register(State.Loading, Loading_Enter, null, null);
             _stateMachine.Register(State.InGame, InGame_Enter, null, null);
 
             _stateMachine.ChangeState(State.Loading);
         }
 
+        // --------------------------------------------------------------------------------------------
+        private void Update()
+        {
+            _stateMachine.Update(Time.deltaTime);
+        }
+
         #region State Machine
 
-        private void LoadingEnter()
+        // --------------------------------------------------------------------------------------------
+        private void Loading_Enter()
         {
             // TODO: actually load stuff
 
             _stateMachine.ChangeState(State.InGame);
         }
 
+        // --------------------------------------------------------------------------------------------
         private void InGame_Enter()
         {
             Debug.Log("InGame_Enter");
