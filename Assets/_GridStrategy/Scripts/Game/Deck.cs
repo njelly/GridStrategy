453d34df6445bf95e88b6623f738d9f2e525d213
Nathaniel Ellingson <nathaniel.j.ellingson@gmail.com>
2020-01-19 01:43:29 -0700

    created Deck.cs

---
+++ Assets/_GridStrategy/Scripts/Game/Deck.cs
@@ -0,0 +1,25 @@
+////////////////////////////////////////////////////////////////////////////////
+//
+//  Deck (c) 2020 Tofunaut
+//
+//  Created by Nathaniel Ellingson for TofuUnity on 01/15/2020
+//
+////////////////////////////////////////////////////////////////////////////////
+
+using Tofunaut.GridStrategy.UI;
+using Tofunaut.SharpUnity.UI;
+
+namespace Tofunaut.GridStrategy.Game
+{
+    public class Deck
+    {
+        public Player Owner { get; private set; }
+
+        private readonly DeckData _deckData;
+
+        public Deck(DeckData deckData, Player owner)
+        {
+            _deckData = deckData;
+        }
+    }
+}
